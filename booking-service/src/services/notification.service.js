0aa51aee4d0978901f756d6e718422b53b9f55a8
Daniel Echegaray <danielechegaray@Daniels-MacBook-Pro-2.local>
2020-12-01 20:40:06 -0500

    change vars

---
+++ booking-service/src/services/notification.service.js
@@ -0,0 +1,19 @@
+const supertest = require("supertest");
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+process.env.NODE_TLS_ACCEPT_UNTRUSTED_CERTIFICATES_THIS_IS_INSECURE = "1";
+
+module.exports = (payload) => {
+  return new Promise((resolve, reject) => {
+    supertest(process.env.NOTIFICATION_API_HOST)
+      .post("/notification/sendEmail")
+      .send({ payload })
+      .end((err, res) => {
+        if (err) {
+          reject(
+            new Error("An error occured with the payment service, err: " + err)
+          );
+        }
+        resolve(res.body);
+      });
+  });
+};
