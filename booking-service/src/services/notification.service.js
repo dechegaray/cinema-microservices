31bfa06398c42f9050a25f8a19f157d18fe40662
Daniel Echegaray <danielechegaray@Daniels-MacBook-Pro-2.local>
2020-12-04 15:46:33 -0500

    Remove unnecessary ENVs and add logs

--- booking-service/src/services/notification.service.js
+++ booking-service/src/services/notification.service.js
@@ -1,6 +1,4 @@
 const supertest = require("supertest");
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-process.env.NODE_TLS_ACCEPT_UNTRUSTED_CERTIFICATES_THIS_IS_INSECURE = "1";
 
 module.exports = (payload) => {
   return new Promise((resolve, reject) => {
@@ -13,6 +11,7 @@ module.exports = (payload) => {
             new Error("An error occured with the payment service, err: " + err)
           );
         }
+        console.log("SHIPA: Sucessfully called notifications!");
         resolve(res.body);
       });
   });
--- booking-service/src/services/payment.service.js
+++ booking-service/src/services/payment.service.js
@@ -1,6 +1,4 @@
 const supertest = require("supertest");
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-process.env.NODE_TLS_ACCEPT_UNTRUSTED_CERTIFICATES_THIS_IS_INSECURE = "1";
 
 module.exports = (paymentOrder) => {
   return new Promise((resolve, reject) => {
@@ -13,6 +11,8 @@ module.exports = (paymentOrder) => {
             new Error("An error occured with the payment service, err: " + err)
           );
         }
+        console.log("SHIPA: Sucessfully called payment endpoint!");
+
         resolve(res.body.paid);
       });
   });
