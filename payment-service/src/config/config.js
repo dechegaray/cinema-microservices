61918849c5368951e72b6530c0242294a487f918
Daniel Echegaray <danielechegaray@Daniels-MacBook-Pro-2.local>
2020-12-19 20:09:53 -0500

    Remove replicasets from db params

--- payment-service/src/config/config.js
+++ payment-service/src/config/config.js
@@ -1,38 +1,8 @@
 const dbSettings = {
-  db: process.env.DB || "payment",
-  user: process.env.DB_USER || "cristian",
-  pass: process.env.DB_PASS || "cristianPassword2017",
-  repl: process.env.DB_REPLS || "rs1",
-  servers: process.env.DB_SERVERS
-    ? process.env.DB_SERVERS.split(" ")
-    : ["192.168.99.100:27017", "192.168.99.101:27017", "192.168.99.102:27017"],
-  dbParameters: () => ({
-    w: "majority",
-    wtimeout: 10000,
-    j: true,
-    readPreference: "ReadPreference.SECONDARY_PREFERRED",
-    native_parser: false,
-  }),
-  serverParameters: () => ({
-    autoReconnect: true,
-    poolSize: 10,
-    socketoptions: {
-      keepAlive: 300,
-      connectTimeoutMS: 30000,
-      socketTimeoutMS: 30000,
-    },
-  }),
-  replsetParameters: (replset = "rs1") => ({
-    replicaSet: replset,
-    ha: true,
-    haInterval: 10000,
-    poolSize: 10,
-    socketoptions: {
-      keepAlive: 300,
-      connectTimeoutMS: 30000,
-      socketTimeoutMS: 30000,
-    },
-  }),
+  db: process.env.DB, // "payment"
+  user: process.env.DB_USER,
+  pass: process.env.DB_PASS,
+  server: process.env.DB_SERVER,
 };
 
 const serverSettings = {
